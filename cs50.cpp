d40d039c6670c74ae6c86f1db0dbd5ba0fa49991
Jeroen van Harten <Jeroen.vanHarten@outlook.com>
2021-03-23 14:49:09 +0100

    Update cs50 source file

--- cs50.cpp
+++ cs50.cpp
@@ -4,9 +4,9 @@
 using std::string;
 
 // prompt the user for ...
-char get_char(string, char=' '); // a char
-double get_double(string, double=0); // a double
-float get_float(string, float=0); // a float
-int get_int(string, int=0); // an int
-long get_long(string, long=0); // a long int
-string get_string(string, string=""); // a string
+char get_char(string, char c); // a char
+double get_double(string, double d); // a double
+float get_float(string, float f); // a float
+int get_int(string, int i); // an int
+long get_long(string, long l); // a long int
+string get_string(string, string s); // a string
