c2072da8af55ce564d0ee38e90584ffec4d5568b
Jeroen van Harten <Jeroen.vanHarten@outlook.com>
2021-03-23 14:44:18 +0100

    Update cs50 header file

--- cs50.hpp
+++ cs50.hpp
@@ -1,6 +1,16 @@
 #ifndef CS50_HPP
 #define CS50_HPP
 
+#include <iostream>
 
+using std::string;
+
+// prompt the user for ...
+char get_char(string, char=' '); // a char
+double get_double(string, double=0); // a double
+float get_float(string, float=0); // a float
+int get_int(string, int=0); // an int
+long get_long(string, long=0); // a long int
+string get_string(string, string=""); // a string
 
 #endif // CS50.HPP
